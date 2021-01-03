80e110c7e4e494d53a9735ae44e74a29efa0d3cc
leo <plus724254@gmail.com>
2021-01-03 13:33:26 +0800

    - 因azure無法使用chromium核心，改用遠程無頭瀏覽器

--- Backend/PriceCompare/PriceCompare/Services/Scratch/DynamicPageScratchService.cs
+++ Backend/PriceCompare/PriceCompare/Services/Scratch/DynamicPageScratchService.cs
@@ -11,15 +11,19 @@ namespace PriceCompare.Services.Scratch
         public async Task<string> GetWebData(string url)
         {
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-            using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            using var browser = await Puppeteer.ConnectAsync(new ConnectOptions()
             {
-                Headless = true
+                BrowserWSEndpoint = $"wss://chrome.browserless.io?token=7220fb88-0ef3-48d0-941d-055b19dda12b&blockAds"
             });
 
             using var page = await browser.NewPageAsync();
             await page.GoToAsync(url, WaitUntilNavigation.DOMContentLoaded);
+            var content = await page.GetContentAsync();
 
-            return await page.GetContentAsync();   
+            //因browser以秒計費，須盡速關閉
+            browser.Dispose();
+
+            return content;
         }
     }
 }
